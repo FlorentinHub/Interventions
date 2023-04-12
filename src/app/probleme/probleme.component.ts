791ee8e65f24ef5be95bf52a371f1d7003713bd6
FLORENTIN TOUPET <FLORENTINTOUPET@GMAIL.COM>
2023-04-12 13:38:08 -0400

    CharlesASDF

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-probleme',
   templateUrl: './probleme.component.html',
   styleUrls: ['./probleme.component.css']
 })
-export class ProblemeComponent {
+export class ProblemeComponent implements OnInit{
+  problemeForm: FormGroup;
   constructor(private fb: FormBuilder ){}
 
+  ngOnInit(){
+    this.problemeForm=this.fb.group({
+      NomProbleme:['',[Validators.minLength(5)]]
+    });
+  }
 }
